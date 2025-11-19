0390342253e9b2ed4eca60c6b536382fa8bebc19
Annemette M <annemettemygind@gmail.com>
2025-11-19 11:15:50 +0100

    Find korteste streng. Rest-parametre, sammenligner længde i for loop

---
+++ opgave-7.ts
@@ -0,0 +1,22 @@
+// Lav en funktion der tager et vilkårligt antal strenge som parametre
+// og returnerer den korteste af strengene
+
+function kortesteStreng(...strenge: string[]): string {
+  if (strenge.length === 0) {
+    return ""; // eller throw Error, alt efter hvad I vil have
+  }
+
+  let korteste = strenge[0];
+
+  //gennemløber argumenter og sammenligner længde
+  for (const s of strenge) {
+    if (s.length < korteste.length) {
+      korteste = s;
+    }
+  }
+
+  return korteste;
+}
+
+// Eksempel:
+console.log(kortesteStreng("hej", "a", "blomst", "hus")); // "a"
